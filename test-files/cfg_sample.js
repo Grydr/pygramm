8bb3f12843e3cfe43e21e4827b4023abe9218727
Grydr <jalucahyoseno@gmail.com>
2026-04-27 19:19:06 +0700

    combine the cfg test files

--- test-files/cfg_sample.js
+++ test-files/cfg_sample.js
@@ -11,4 +11,30 @@ while (value > 0) {
     value -= 1;
 }
 
+function compute(a, b) {
+    let total = a + b;
+    for (let i = 0; i < 3; i += 1) {
+        total += i;
+    }
+
+    switch (total) {
+        case 0:
+            total = 100;
+            break;
+        case 1:
+            total = 200;
+            break;
+        default:
+            total = 300;
+    }
+
+    return total;
+}
+
+let result = compute(2, 3);
+let typeName = typeof result;
+let dropped = delete result;
+let nothing = void result;
+
+
 return str;
